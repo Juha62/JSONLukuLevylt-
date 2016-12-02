16e8f0ffe83d12ecda67e8e428f6055c7829ad5a
juham <juham@JUHA-X55UR>
2016-12-02 13:34:03 +0200

    Eka versio. Ihan normali tiedoston luk ilman mitään JSON käsittelyö.

---
+++ JSONLukuLevylta/JSONLukuLevylta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONLukuLevylta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Filetsu = @"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETCore\v5.0\project.lock.json";
+
+            if (File.Exists(Filetsu))
+
+
+            {
+
+
+
+                string aiempitiedosto = File.ReadAllText(Filetsu);
+                Console.WriteLine(aiempitiedosto);
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
